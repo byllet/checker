e0df18bb78d3192bdc72c3eac47618bcb17d91af
byllet <1rasim1@list.ru>
2025-11-08 02:05:47 +0300

    feat: Data splited to NetlistData and FileData

--- src/loader.py
+++ src/loader.py
@@ -1,16 +1,37 @@
-from dataclasses import dataclass
 from data import NetlistProject
+from report import Error, ReportEntry
 
 
 def parse(path):
-    return [None, None, None]
-
+    return [None, None, None, {Error.DUPLICATE_NAME: ReportEntry(error=Error.DUPLICATE_NAME)}]
+    
 
 class Data:
-    def __init__(self, data_path : str):
-        self.ast_tree = None
-        self.parse_tree = None
-        self.net_list: NetlistProject = None
+    def __init__(self):
+        self._ast_tree = None
+        self._parse_tree = None
+        self._netlist: NetlistProject = None
+        self._errors: dict[Error, ReportEntry] = {}
 
-        [self.ast_tree, self.parse_tree, self.net_list] = parse(data_path)
+    @property
+    def netlist(self):
+        return self._netlist
 
+    @property
+    def errors(self):
+        return self._errors
+
+
+class FileData(Data):
+    def __init__(self, data_path: str):
+        super().__init__()
+        self.__load_from_file(data_path)
+    
+    def __load_from_file(self, data_path: str):
+        self._ast_tree, self._parse_tree, self._netlist, self._errors = parse(data_path)
+
+
+class NetlistData(Data):
+    def __init__(self, netlist: NetlistProject):
+        super().__init__()
+        self._netlist = netlist
