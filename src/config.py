723bdf6d83ffe17c1d581d6f8dd6f9673bf8f514
byllet <1rasim1@list.ru>
2025-11-10 17:48:44 +0300

    fix: import naming

--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
 from syntax_check import check_syntax
 from network_check import check_network_connection, check_network_correctness
-from hierarchy_loop_and_incompletence import check_cycle_hierarchy, check_incomplete_hierarchy
+from hierarchy_loop_and_incompletence_check import check_cycle_hierarchy, check_incomplete_hierarchy
 from scheme_check import check_pin_connection, check_subcircuit_correctness
 
 
