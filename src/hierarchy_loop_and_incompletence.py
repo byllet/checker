ac53767480dd475a80422e2aad4610d27b5fd407
c4llery <anton.rusanov.04@gmail.com>
2025-12-03 10:08:36 +0300

    feat: добавил сообщение о найденной неполноте иерархии

--- src/hierarchy_loop_and_incompletence.py
+++ src/hierarchy_loop_and_incompletence.py
@@ -6,7 +6,11 @@ from report import Reporter, Error, ReportEntry
 def check_incomplete_hierarchy(data: Data, reporter: Reporter):
     """Проверка на неполноту иерархии"""
     if Error.MISSING_BLOCK in data.errors_after_parse:
-        reporter.add_error(data.errors_after_parse[Error.MISSING_BLOCK])
+        report = ReportEntry(
+                    error = Error.MISSING_BLOCK,
+                    message = f'Missing block detected in Netlist!'
+                )
+        reporter.add_error(report)
         return False
     return True
 
