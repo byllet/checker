617ede3cfb6c234eb8980885469d89d15bb26894
byllet <1rasim1@list.ru>
2025-12-03 22:38:11 +0300

    fix: fxed input type

--- src/cli.py
+++ src/cli.py
@@ -17,8 +17,8 @@ def main(input: str, output: str):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Netlist Checker')
-    parser.add_argument('--input', type=Path, required=True, help='Input netlist file')
-    parser.add_argument('-o', '--output', type=Path, help='Output report file')
+    parser.add_argument('--input', type=str, required=True, help='Input netlist file')
+    parser.add_argument('-o', '--output', type=str, help='Output report file')
 
     args = parser.parse_args()
     main(args.input, args.output)
