6eb145ececa79829ffa04cd9c908b47fb46edad6
c4llery <anton.rusanov.04@gmail.com>
2025-12-05 01:36:50 +0300

    fix: greatest fix ever

--- src/hierarchy_loop_and_incompletence_check.py
+++ src/hierarchy_loop_and_incompletence_check.py
@@ -5,7 +5,7 @@ from report import Reporter, Error, ReportEntry
 
 def check_incomplete_hierarchy(data: Data, reporter: Reporter):
     """Проверка на неполноту иерархии"""
-    if Error.MISSING_BLOCK in data.errors_after_parse:
+    if data.errors_after_parse != {}:
         report = ReportEntry(
                     error = Error.MISSING_BLOCK,
                     message = f'Missing block detected in Netlist!'
